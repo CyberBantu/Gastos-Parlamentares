af3f02a69ca364dff51a17fbf34cccb2f3f3c8d4
Christian Basilio <Christianbasilio97@gmail.com>
2024-02-14 14:34:20 -0300

    Retirada agregadas sem atualização

--- deputados.py
+++ deputados.py
@@ -13,7 +13,8 @@ dados = pd.json_normalize(resp['dados'])
 st.set_page_config(layout="wide")
 
 
-st.title("Painel de Gastos de Deputados do Brasil (2023)")
+st.title("Painel de Gastos de Deputados do Brasil (2023 - 24)")
+st.caption('Fonte - Camara dos Deputados / Elaborado por Christian Basilio - Dados Marginais')
 
 # Criando um selecionados
 nomes_unicos = dados["nome"].unique()
@@ -70,76 +71,20 @@ if not tabela_deputado.empty:
         st.text(f"UF: {dados['email'].iloc[0]}")
         st.image(url_foto, width=150)
 
-        # Adicionando um card com a soma do valorLiquido
+        # card com a soma do valorLiquido
         total_valor_liquido = dados_gastos['Valor Líquido'].str.replace('R$', '').str.replace(',', '').astype(float).sum()
         st.info(f"Total Valor Líquido: R$ {total_valor_liquido:.2f}")
 
-        # Adicionando um card com a soma do valorLiquido por Tipo de Despesa
+        # Card de valor liquido
         sum_by_tipo_despesa = dados_gastos.groupby('Tipo de Despesa')['Valor Líquido'].apply(lambda x: x.str.replace('R$', '').str.replace(',', '').astype(float).sum())
         st.info("Total Valor Líquido por Tipo de Despesa:")
         for tipo_despesa, total_valor in sum_by_tipo_despesa.items():
             st.info(f"{tipo_despesa}: R$ {total_valor:.2f}")
 
-        st.table(dados_gastos)  # Exibe apenas os primeiros 10 registros, ajuste conforme necessário
+        st.dataframe(dados_gastos)  
     else:
         st.warning(f"Não há dados de gastos para o deputado {deputy_name}")
 else:
     st.warning(f"Não há informações para o deputado {deputy_name}")
 
-st.title('Painel de Agregação de Gastos')
-st.text('Informações Agregadas por Partidos')
-
-# Base de 02 de dezembro 2023 ------------------------------------
-# Baixando a base 
-base_0212 = pd.read_excel('base_gasto_parlamentar_0212.xlsx')
-
-
-# juntando com dados
-
-base_principal_0212 = base_0212.merge(dados, how='left', left_on='IdDeputado',right_on = 'id')
-
-# calculo de gasto por partido
-# Gasto por Partido
-gasto_partido = base_principal_0212.groupby('siglaPartido')['valorLiquido'].agg(['sum', 'mean']).reset_index().sort_values(by='sum', ascending=True)
-
-gasto_estado = base_principal_0212.groupby('siglaUf')['valorLiquido'].agg(['sum', 'mean']).reset_index().sort_values(by='sum', ascending=True)
-
-
-# Fazendo a agregação
-gasto_partido = (
-    base_principal_0212.groupby('siglaPartido')['valorLiquido']
-    .agg(['sum', 'mean'])
-    .reset_index()
-    .sort_values(by='sum', ascending=False)
-)
-
-# Renomear as colunas
-gasto_partido.columns = ['siglaPartido', 'Total', 'Média']
-
-# Formatação em reais
-def em_reais(valor):
-    return f'R$ {valor:.2f}'
-
-# Fazendo a contagem de partios
-count_partido = dados['siglaPartido'].value_counts()
-
-base_consolidada = pd.merge(gasto_partido, count_partido, left_on='siglaPartido', right_index=True).rename(columns ={'count' : 'Total de Parlamentares'})
-
-# Ultimas alterações
-base_consolidada['Média de Gastos por Partido'] = base_consolidada['Total'] / base_consolidada['Total de Parlamentares']
-
-
-base_consolidada['Total'] = base_consolidada['Total'].apply(em_reais)
-base_consolidada['Média'] = base_consolidada['Média'].apply(em_reais)
-base_consolidada['Média de Gastos por Partido'] = base_consolidada['Média de Gastos por Partido'].apply(em_reais)
-
-st.table(base_consolidada)
-
-st.text('Gasto Médio de Despesa de uso Parlamentar por Partido em 2023')
-iframe_grafico_partido_media = "https://datawrapper.dwcdn.net/9nkSc/3/"
-st.components.v1.iframe(iframe_grafico_partido_media, width=1366, height=700)
-
-st.text('Média de Gastos de parlamentares por total de integrantes do Partido')
-iframe_media_por_total_partido = 'https://datawrapper.dwcdn.net/jn81k/1/'
-st.components.v1.iframe(iframe_media_por_total_partido, width=1366, height = 700)
-
+st.caption('Elaborado por Christian Basilio (Dados Marginais)')
